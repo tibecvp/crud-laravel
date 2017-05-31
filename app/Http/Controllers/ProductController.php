21d4f44812d188968ad1203372eafc743778e67a
tibecvp <tibecvp@gmail.com>
2017-05-30 21:56:28 -0500

    Se implementa el listado de productos y la vista individual para cada producto

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,19 @@
 namespace Tibecrud\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Tibecrud\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+    	$products = Product::orderBy('id', 'DESC')->paginate();
+    	return view('products.index', compact('products')); 
+    }
+
+    public function show($id)
+    {
+    	$product = Product::find($id);
+    	return view('products.show', compact('product'));
+    }
 }
